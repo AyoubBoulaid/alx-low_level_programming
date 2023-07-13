b481f231423ed6d379ef9a411b75d20d5aa80ae6
Ayoub <boulaidayoub02@gmail.com>
2023-07-13 13:31:52 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*main - Entry point of the program
+*return The status code indicating the success or failure of the program
+*/
+int main(void)
+{
+	puts("\"Programming is like multilingual puzzle");
+	return (0);
+}
