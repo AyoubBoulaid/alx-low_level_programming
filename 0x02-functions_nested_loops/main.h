104ecf9cb270955f3b84a2a6680ce235ad306f91
Ayoub <boulaidayoub02@gmail.com>
2023-07-18 13:00:06 -0700

    C for loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,3 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
 int _putchar(char);
 void print_alphabet(void);
@@ -20,7 +11,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);	
-{
-	return (write(1, &c, 1));
-}
+void print_times_table(int n);
