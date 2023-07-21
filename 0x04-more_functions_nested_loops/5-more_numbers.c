9f7a15049d2925585d14d3176e46c5b76d19bcfb
Ayoub <boulaidayoub02@gmail.com>
2023-07-21 09:00:12 -0700

    More C and way more to C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14 ten times
+ *
+ * Return: void due to void function
+ */
+void more_numbers(void)
+{
+	int r,n; /* r=repition and n=numbers */
+
+	for (r = 0; r <= 10; r++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			_putchar('1');
+			_putchar (n % 10 + '0');
+		}
+	}
+	_putchar('\n');
+}
