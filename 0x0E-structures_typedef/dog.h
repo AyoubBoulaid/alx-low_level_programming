3e623ac73f6ee3c006ac1cf21a89a4c701e12416
Ayoub <boulaidayoub02@gmail.com>
2023-08-14 17:59:49 -0700

    PreProcessor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,72 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog's basic information
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure holds the basic information about a dog.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+/**
+ * dog_t - A typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * init_dog - Initializes a dog structure
+ * @d: A pointer to the dog structure
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints the information of a dog
+ * @d: A pointer to the dog structure
+ */
+void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog structure
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: A pointer to the newly created dog structure
+ */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - Frees the memory allocated for a dog structure
+ * @d: A pointer to the dog structure
+ */
+void free_dog(dog_t *d);
+
+/**
+ * _strcpy - Copies a string
+ * @dest: The destination buffer
+ * @src: The source string
+ *
+ * Return: A pointer to the destination buffer
+ */
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strlen - Calculates the length of a string
+ * @s: The input string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s);
+
+#endif /* DOG_H */
