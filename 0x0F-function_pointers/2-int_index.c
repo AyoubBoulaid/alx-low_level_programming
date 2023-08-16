06f7772635a197a4d22e537c20277f0d4ebc28cb
Ayoub <boulaidayoub02@gmail.com>
2023-08-16 12:44:02 -0700

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * int_index - Returns the index of the first element that matches the condition
+ * int_index - Returns the index of the first
+ * element that matches the condition
  * @array: Array to search
  * @size: Number of elements in the array
  * @cmp: Pointer to the comparison function
@@ -10,16 +11,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-			return i;
+			return (i);
 	}
 
-	return -1;
+	return (-1);
 }
