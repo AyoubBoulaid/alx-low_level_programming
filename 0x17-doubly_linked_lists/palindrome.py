ac47425cde29377d058346b387d0448cf608631d
Ayoub <boulaidayoub02@gmail.com>
2024-03-21 10:08:17 -0700

    Back to C doubly linked lists

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+max_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        product = i * j
+        if is_palindrome(product) and product > max_palindrome:
+            max_palindrome = product
+
+with open("result", "w") as file:
+    file.write(str(max_palindrome))
