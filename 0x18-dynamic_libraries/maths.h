548fc21d114609b21221c1d9340534f851c11c89
Ayoub <boulaidayoub02@gmail.com>
2024-03-25 08:37:06 -0700

    DYnamic Libs

---
+++ 0x18-dynamic_libraries/maths.h
@@ -0,0 +1,11 @@
+#ifndef MATHS_H
+#define MATHS_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+int power(int base, int exponent);
+
+#endif /* MATHS_H */
+
